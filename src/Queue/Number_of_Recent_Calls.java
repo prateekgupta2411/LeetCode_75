ad558653c3480fdba7feb6a1e39e2b78c60f49b2
Prateek Gupta <68748725+prateekgupta2411@users.noreply.github.com>
2025-03-05 23:12:33 +0530

    Queue

---
+++ src/Queue/Number_of_Recent_Calls.java
@@ -0,0 +1,20 @@
+package Queue;
+
+public class Number_of_Recent_Calls {
+    public static void main(String[] args) {
+        //    class RecentCounter {
+//        Queue<Integer> queue;
+//        public RecentCounter() {
+//            queue = new LinkedList();
+//        }
+//
+//        public int ping(int t) {
+//            queue.add(t);
+//            while(!queue.isEmpty() && t - 3000 > queue.peek()){
+//                queue.poll();
+//            }
+//            return queue.size();
+//        }
+//    }
+    }
+}
